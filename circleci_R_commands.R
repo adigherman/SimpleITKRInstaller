1d93f050468b32baff1b7573e827b4237f1eed17
Richard Beare <Richard.Beare@gmail.com>
2016-09-07 07:48:15 +1000

    incomplete ld stuff

--- circleci_R_commands.R
+++ circleci_R_commands.R
@@ -27,7 +27,7 @@ Sys.setenv(CC = paste("distcc", CC),
            CXX= paste("distcc", CXX),
            RTESTON="ON",
            MAKEJ=makej,
-           DCMAKE_LINKER=paste("distcc", "ld"),
+           DCMAKE_LINKER=paste("-DCMAKE_LINKER=distcc", "ld"),
            DITK_REPOSITORY=paste0("-DITK_REPOSITORY=", ITKREPO))
 
 devtools::install("/home/ubuntu/SimpleITKRInstaller")
