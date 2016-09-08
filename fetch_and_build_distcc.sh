88a417277a8f619a1a13e1abcdffa7c8a1af29e4
Richard Beare <richard.beare@monash.edu>
2016-09-08 11:27:20 +1000

    Forgot to change to distcc folder

--- fetch_and_build_distcc.sh
+++ fetch_and_build_distcc.sh
@@ -4,9 +4,10 @@ if [ -d "${DISTCC_SRC}" ] ; then
 
   cd ${DISTCC_SRC} &&
   [ -d distcc ] || (
-  git clone https://github.com/distcc/distcc
-  git checkout 3.2
-  ./configure
+  git clone https://github.com/distcc/distcc &&
+  cd distcc &&
+  git checkout 3.2 &&
+  ./configure &&
   make )
 else
     echo "Missing ${DISTCC_SRC}"
