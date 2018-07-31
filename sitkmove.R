fc47d8e4bb2b764a0adc9e3b5f20c7cbe5a12898
Richard Beare (Monash University) <rbeare@m3f004.massive.org.au>
2018-07-31 19:42:20 +1000

    experiments with packaging alternatives

--- sitkmove.R
+++ sitkmove.R
@@ -1,15 +1,29 @@
 movestuff <- function(builddir, destdir)
 {
     pkgstuff <- list.files(path=builddir, full.names=TRUE)
+
     bn <- basename(pkgstuff)
     fulldest <- file.path(destdir, bn)
-    file.rename(from=pkgstuff, to=fulldest)
+    alreadyexists <- file.exists(fulldest)
+    file.rename(from=pkgstuff[!alreadyexists], to=fulldest[!alreadyexists])
 }
+
 
 args <- commandArgs( TRUE )
 src <- args[[1]]
 dest <- args[[2]]
+fullsource <- as.logical(as.numeric(args[[3]]))
+if (!fullsource) {
+    ## move folders that don't exist (i.e) not the src folder
+    movestuff(src, dest)
+}
+## copy the makefile
+file.copy(file.path(src, "src/Makefile"), file.path(dest, "src"))
+## copy the DESCRIPTION
+file.copy(file.path(src, "DESCRIPTION"), file.path(dest, "DESCRIPTION"))
 
-movestuff(src, dest)
+## move the library
+file.rename(file.path(src, "src/SimpleITK.so"), file.path(dest, "src/SimpleITK.so"))
+
 q(save="no")
 
